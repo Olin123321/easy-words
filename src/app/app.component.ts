3c31a805ac6a006012f177b3f5f52b018111d679
Olin <61127141+Olin123321@users.noreply.github.com>
2022-04-19 00:15:41 +0200

    Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,9 @@ import { WordsService } from './services/words.service';
 })
 export class AppComponent {
   
+  nouns = this.wordsService.getNouns();
+  verbs = this.wordsService.getVerbs();
+
   constructor(private wordsService: WordsService) { }
 
-  get nouns(){
-    return this.wordsService.getNouns();
-  }
-
-  get verbs(){
-    return this.wordsService.getVerbs();
-  }
-    
 }
--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { WordType } from 'src/app/data/models';
 import { WordsService } from 'src/app/services/words.service';
 
@@ -7,14 +8,23 @@ import { WordsService } from 'src/app/services/words.service';
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
-export class QuestionComponent implements OnInit {
+export class QuestionComponent implements OnInit, OnDestroy {
 
-  word: WordType | undefined;
+  word: WordType;
+  private words: WordType[] = [];
+  private subscribtion?: Subscription;
 
   constructor(private wordsService: WordsService) { }
 
+  ngOnDestroy(): void {
+    this.subscribtion.unsubscribe();
+  }
+
   ngOnInit(): void {
-    this.fetchWord();
+    this.subscribtion = this.wordsService.getWords().subscribe((words: WordType[]) => {
+      this.words = words;
+      this.fetchWord();
+    });
   }
 
   addToNouns(word: WordType): void {
@@ -27,11 +37,7 @@ export class QuestionComponent implements OnInit {
     this.fetchWord();
   }
 
-  check(): void {
-    this.wordsService.check();
-  }
-
   private fetchWord(): void {
-    this.word = this.wordsService.getWords().shift();
+    this.word = this.words.shift();
   }
 }
